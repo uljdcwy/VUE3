f75bf959108aa395a4e78d354dc43b1cca109691
曾细亚 <uljdcwykhgm@outlook.com>
2023-07-12 22:33:21 +0800

    2023-7-12 转换悬念

--- packages/compiler-ssr/src/transforms/ssrTransformSlotOutlet.ts
+++ packages/compiler-ssr/src/transforms/ssrTransformSlotOutlet.ts
@@ -15,9 +15,11 @@ import {
   SSRTransformContext,
   processChildrenAsStatement
 } from '../ssrCodegenTransform'
-
+// SSR 转换 插糟输入的声名
 export const ssrTransformSlotOutlet: NodeTransform = (node, context) => {
+  // 如果节点是插糟
   if (isSlotOutlet(node)) {
+    // 解构slot 的名称与属性
     const { slotName, slotProps } = processSlotOutlet(node, context)
 
     const args = [
@@ -31,7 +33,9 @@ export const ssrTransformSlotOutlet: NodeTransform = (node, context) => {
     ]
 
     // inject slot scope id if current template uses :slotted
+    // 上下文对象的作用域ID与slot不为false
     if (context.scopeId && context.slotted !== false) {
+      // 压入作用域id
       args.push(`"${context.scopeId}-s"`)
     }
 
@@ -55,11 +59,11 @@ export const ssrTransformSlotOutlet: NodeTransform = (node, context) => {
       }
       args.push('true')
     }
-
+    // 节点的SSR编码内容指向创建的表达式
     node.ssrCodegenNode = createCallExpression(context.helper(method), args)
   }
 }
-
+// SSR 插糟输出声名
 export function ssrProcessSlotOutlet(
   node: SlotOutletNode,
   context: SSRTransformContext
@@ -67,20 +71,25 @@ export function ssrProcessSlotOutlet(
   const renderCall = node.ssrCodegenNode!
 
   // has fallback content
+  // 如果子节点为真
   if (node.children.length) {
+    // 创建空的函数表达式
     const fallbackRenderFn = createFunctionExpression([])
+    // 函数表达式内容反映向返回内容
     fallbackRenderFn.body = processChildrenAsStatement(node, context)
     // _renderSlot(slots, name, props, fallback, ...)
+    // 渲染可执行参数的第三个指向函数表达式
     renderCall.arguments[3] = fallbackRenderFn
   }
 
   // Forwarded <slot/>. Merge slot scope ids
+  // 上下文对象的作用域ID为真时
   if (context.withSlotScopeId) {
     const slotScopeId = renderCall.arguments[6]
     renderCall.arguments[6] = slotScopeId
       ? `${slotScopeId as string} + _scopeId`
       : `_scopeId`
   }
-
+   // 上下文对象中压入SSR节点编码
   context.pushStatement(node.ssrCodegenNode!)
 }
--- packages/compiler-ssr/src/transforms/ssrTransformSuspense.ts
+++ packages/compiler-ssr/src/transforms/ssrTransformSuspense.ts
@@ -25,18 +25,23 @@ interface WIPEntry {
 }
 
 // phase 1
+// SSR 创建一个闭包存丰节点与上下文对象，并返回函数
 export function ssrTransformSuspense(
   node: ComponentNode,
   context: TransformContext
 ) {
   return () => {
+    // 如果有子节喽
     if (node.children.length) {
       const wipEntry: WIPEntry = {
         slotsExp: null!, // to be immediately set
         wipSlots: []
       }
+      // 弱引用中设置节点键指向 入口
       wipMap.set(node, wipEntry)
+      // 入口slot表达式为构建的slot 
       wipEntry.slotsExp = buildSlots(node, context, (_props, children, loc) => {
+        // 创建函数表达式
         const fn = createFunctionExpression(
           [],
           undefined, // no return, assign body later
@@ -44,6 +49,7 @@ export function ssrTransformSuspense(
           false, // suspense slots are not treated as normal slots
           loc
         )
+        // 入口的wipSlots 压入函数与子元素
         wipEntry.wipSlots.push({
           fn,
           children
@@ -55,21 +61,27 @@ export function ssrTransformSuspense(
 }
 
 // phase 2
+// 进程悬念
 export function ssrProcessSuspense(
   node: ComponentNode,
   context: SSRTransformContext
 ) {
   // complete wip slots with ssr code
+  // 获取节点对应的value
   const wipEntry = wipMap.get(node)
   if (!wipEntry) {
     return
   }
+  // 解构slot表达式与wipSlot
   const { slotsExp, wipSlots } = wipEntry
+  // 循环SLOT 
   for (let i = 0; i < wipSlots.length; i++) {
     const slot = wipSlots[i]
+    // 处理子元素声名
     slot.fn.body = processChildrenAsStatement(slot, context)
   }
   // _push(ssrRenderSuspense(slots))
+  // 上下文对象中的 声名压入创建的表达式
   context.pushStatement(
     createCallExpression(context.helper(SSR_RENDER_SUSPENSE), [
       `_push`,
